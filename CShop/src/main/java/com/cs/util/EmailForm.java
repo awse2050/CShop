219810b9ef1bc9eefe3e6c8758314395ef12273a
kyh29 <kyh3964@naver.com>
2021-04-15 15:12:52 +0900

    Add EmailSendService

---
+++ CShop/src/main/java/com/cs/util/EmailForm.java
@@ -0,0 +1,12 @@
+package com.cs.util;
+
+import lombok.Data;
+
+@Data
+public class EmailForm {
+	
+	private String to;
+	private String subject;
+	private String text;
+	
+}
---
+++ CShop/src/test/java/com/cs/service/EmailSendServiceTests.java
@@ -0,0 +1,32 @@
+package com.cs.service;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import com.cs.util.EmailForm;
+
+import lombok.extern.log4j.Log4j;
+
+@Log4j
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration({"file:src/main/webapp/WEB-INF/spring/root-context.xml", 
+	"file:src/main/webapp/WEB-INF/spring/security-context.xml"})
+public class EmailSendServiceTests {
+	
+	@Autowired
+	private EmailSendService service;
+	
+	@Test
+	public void sendTest() {
+		EmailForm emailForm = new EmailForm();
+		emailForm.setTo("kyh3964@naver.com");
+		emailForm.setSubject("안녕하세요?");
+		emailForm.setText("반가워요!!");
+		
+		log.info(service.sendEmailToUser(emailForm));
+	}
+
+}
